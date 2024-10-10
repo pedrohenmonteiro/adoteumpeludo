a792dfd113a440de4139bbb3b04b3f3db807cc0c
Pedro Monteiro <pedromont436@gmail.com>
2024-10-10 19:47:21 -0300

    criando header da pagina

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import Button from "../Button/Button";
+import Container from "../Container/Container";
 
 export const Header = () => {
   return (
     <header className="w-full bg-indigo-400">
-      <h1 className="text-white font-bold">Adote um peludo</h1>
-      <div>
-        <Button>Teste</Button>
-      </div>
+      <Container className="flex justify-between items-center">
+        <h1 className="text-white font-bold">Adote um peludo</h1>
+        <div>
+          <Button>Teste</Button>
+        </div>
+      </Container>
     </header>
   );
 };
